7bba29a2baf0922f4e2413342cec58502e238aed
luqman499 <luqmanjaffary79@gmail.com>
2026-06-14 14:37:56 +0500

    Learn about the Routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,3 @@
 <h1>Wellcome to laravel</h1>
 <h2>This is the last time i started the laravel again and this time. I will do my best to do in the laravel</h2>
+<h1>Wellcome page by Default</h1>
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,66 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/hell', function () {
-    return view('about');
+// Route::get('/hell', function () {
+//     return view('about');
+// });
+
+// Route::view('/about', 'about');
+
+
+// Route::get('/', function () {
+// $person = [
+//     'name' => 'Ali',
+//     'email' => 'luqman@gmail.com'
+// ];
+// dd($person);
+//     return view('welcome');
+// });
+
+//Route Parameters
+
+// Route::get('/product/{id}', function (string $id) {
+//     return "Product ID = $id";
+// });
+
+
+// Route::get('/{lang}/product/{id}', function (string $lang, string $id) {
+//     return "Lang($lang) PRODUCT ID($id)";
+// });
+
+//Optional Parameters
+
+// Route::get('/product/{category?}', function (string $category = null) {
+//     return "Category For Product= {$category}";
+// });
+
+//Route Validation
+
+// Route::get('/product/{id}', function (string $id) {
+//     return "Work! {$id}";
+// })->whereNumber('id');
+// whereAlpha
+//whereIn(id, ['12', '12', '15']);
+
+//Name Route
+
+// Route::get('/', function () {
+//     $aboutPageUrl = route('about');
+//     dd($aboutPageUrl);
+
+//     return view('welcome');
+// });
+
+// Route::view('/about-us', 'about')->name('about');
+
+//Group Route
+
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        return "admin/users";
+    });
 });
 
-
-Route::get('/', function () {
-    return view('welcome');
+Route::fallback(function () {
+    return "This Page is Not Found !!!!";
 });
-
-
