588b4f81d37ab1787f37bf8f87a51d4f0a5cadeb
luqman499 <luqmanjaffary79@gmail.com>
2026-06-20 12:01:53 +0500

    update the layout files

--- resources/views/layouts/clean.blade.php
+++ resources/views/layouts/clean.blade.php
@@ -24,6 +24,9 @@
 
 <body>
     @yield('childContent')
+
+
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js"
         integrity="sha512-XJgPMFq31Ren4pKVQgeD+0JTDzn0IwS1802sc+QTZckE6rny7AN2HLReq6Yamwpd2hFe5nJJGZLvPStWFv5Kww=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
