941e79e0e8472706926aa02e4cf1c58b96e2ba07
luqman499 <luqmanjaffary79@gmail.com>
2026-06-20 17:42:48 +0500

    create the login  and signup page

--- resources/views/layouts/clean.blade.php
+++ resources/views/layouts/clean.blade.php
@@ -22,15 +22,17 @@
     <!-- <link rel="stylesheet" href="css/output.css" /> -->
 </head>
 
-<body>
-    @yield('childContent')
+{{-- <body class="{{ $cssClass }}"> --}}
+
+<body class="@isset($cssClass){{ $cssClass }}@endisset"></body>
+@yield('childContent')
 
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js"
-        integrity="sha512-XJgPMFq31Ren4pKVQgeD+0JTDzn0IwS1802sc+QTZckE6rny7AN2HLReq6Yamwpd2hFe5nJJGZLvPStWFv5Kww=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="js/app.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js"
+    integrity="sha512-XJgPMFq31Ren4pKVQgeD+0JTDzn0IwS1802sc+QTZckE6rny7AN2HLReq6Yamwpd2hFe5nJJGZLvPStWFv5Kww=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="js/app.js"></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SingupController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::view('/', 'index');
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/signup', [SingupController::class, 'create']);
+Route::get('/login', [LoginController::class, 'create']);
